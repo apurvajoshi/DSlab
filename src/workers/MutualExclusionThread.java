8bb14f1c715fe277a979c0bc1a21908674060056
U-Gouri-PC\Gouri <Gouri@Gouri-PC.(none)>
2013-02-13 04:03:42 -0500

    updated receive processing mutex

--- src/workers/MutualExclusionThread.java
+++ src/workers/MutualExclusionThread.java
@@ -1,6 +1,7 @@
 package workers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import manager.MessagePasser;
 import model.TimeStampedMessage;
@@ -8,9 +9,15 @@ import model.TimeStampedMessage;
 public class MutualExclusionThread extends Thread {
 
 	private List<TimeStampedMessage> threadRcvQueue;
+	private ArrayList<TimeStampedMessage> mutexQueue;
+	private boolean criticalSection;
+	private boolean voted;
 
 	public MutualExclusionThread(List<TimeStampedMessage> threadRcvQueue) throws IOException {
 		this.threadRcvQueue = threadRcvQueue;
+		this.criticalSection = false;
+		this.voted = false;
+		this.mutexQueue = new ArrayList<TimeStampedMessage>();
 	}
 	
 	public void run() {
@@ -22,17 +29,17 @@ public class MutualExclusionThread extends Thread {
 			while(!this.threadRcvQueue.isEmpty())
 			{
 				TimeStampedMessage m = this.threadRcvQueue.remove(0);
-				MessagePasser.getInstance().addToProcessQueue(m);
+				MessagePasser.getInstance().addToProcessQueue(m); //apply rules
 				while(!MessagePasser.getInstance().processQueue.isEmpty())
 				{
 					TimeStampedMessage msg = MessagePasser.getInstance().processQueue.remove(0);
-					
-					
+										
 					/* YOUR CODE COMES HERE */
 					/* Possibly a function call */
-					
-					
 					MessagePasser.getInstance().clockService.resyncTimeStamp(msg.getTimeStamp());
+					validateMessage(msg);
+										
+					/*
 					if(MessagePasser.getInstance().clockService.getClass().getSimpleName().equals("LogicalClock"))
 					{
 						MessagePasser.getInstance().clockService.increment(0);
@@ -42,10 +49,40 @@ public class MutualExclusionThread extends Thread {
 						MessagePasser.getInstance();
 						MessagePasser.getInstance().clockService.increment(MessagePasser.getInstance().nodes.indexOf(MessagePasser.getInstance().findNodeByName(MessagePasser.localName)));
 					}
-					MessagePasser.getInstance().rcvQueue.add(msg);
+					*/
+					
 				}
 			}
+		}		
+	}
+
+	private void validateMessage(TimeStampedMessage msg) {
+		if (msg.getKind().equals("mreq")) {
+			if (criticalSection || voted) {
+				mutexQueue.add(msg);
+			}
+			else {
+				//send(mack);
+				voted = true;
+			}
+			
 		}
+		else if (msg.getKind().equals("mrel")) {
+			if (mutexQueue.size() == 0) {
+				voted = false;
+			}
+			else {
+				//send MACK to process with lowest TS in mutexQ
+				voted = true;
+			}
+		}
+		else if (msg.getKind().equals("mack")) {
+			ackCounter++;
+			
+		}
+		else {
+			MessagePasser.getInstance().rcvQueue.add(msg);
+		}		
 	}
 	
 }
