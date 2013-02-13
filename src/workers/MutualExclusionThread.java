ba6cedcb86947a47a5167c16a81bccec83c81c52
Apurva Joshi <apurvaj@andrew.cmu.edu>
2013-02-13 03:06:08 -0500

    New File

---
+++ src/workers/MutualExclusionThread.java
@@ -0,0 +1,51 @@
+package workers;
+
+import java.io.IOException;
+import java.util.List;
+import manager.MessagePasser;
+import model.TimeStampedMessage;
+
+public class MutualExclusionThread extends Thread {
+
+	private List<TimeStampedMessage> threadRcvQueue;
+
+	public MutualExclusionThread(List<TimeStampedMessage> threadRcvQueue) throws IOException {
+		this.threadRcvQueue = threadRcvQueue;
+	}
+	
+	public void run() {
+		while (true) 
+		{
+			/* Wait for the threadRcvQueue to get filled */
+			while(this.threadRcvQueue.isEmpty());
+			
+			while(!this.threadRcvQueue.isEmpty())
+			{
+				TimeStampedMessage m = this.threadRcvQueue.remove(0);
+				MessagePasser.getInstance().addToProcessQueue(m);
+				while(!MessagePasser.getInstance().processQueue.isEmpty())
+				{
+					TimeStampedMessage msg = MessagePasser.getInstance().processQueue.remove(0);
+					
+					
+					/* YOUR CODE COMES HERE */
+					/* Possibly a function call */
+					
+					
+					MessagePasser.getInstance().clockService.resyncTimeStamp(msg.getTimeStamp());
+					if(MessagePasser.getInstance().clockService.getClass().getSimpleName().equals("LogicalClock"))
+					{
+						MessagePasser.getInstance().clockService.increment(0);
+					}
+					else 
+					{
+						MessagePasser.getInstance();
+						MessagePasser.getInstance().clockService.increment(MessagePasser.getInstance().nodes.indexOf(MessagePasser.getInstance().findNodeByName(MessagePasser.localName)));
+					}
+					MessagePasser.getInstance().rcvQueue.add(msg);
+				}
+			}
+		}
+	}
+	
+}
